9e59853b0bd508698899552d1d3b81f5fe693fa3
nataliaermeryd <natalia.ermeryd@outlook.com>
2022-02-28 20:47:25 +0100

    Basics

---
+++ src/Music.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Music() {
+  return (
+    <div className="Music">
+        <h1>Music Page</h1>
+    </div>
+  );
+}
+
+export default Music;
---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function Nav() {
+  return (
+        <nav>
+            <h3>LOGO</h3>
+            <ul>
+                <li>ABOUT</li>
+                <li>MUSIC</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
