046f5a151f65fe617e5d84118543549150e4e9c3
nataliaermeryd <natalia.ermeryd@outlook.com>
2022-03-15 00:15:05 +0100

    Contactform Ready

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useState } from "react";
 import './ContactForm.css';
 
+const ContactForm = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const handleSubmit = () => {
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 100);
+  };
 
-function ContactForm() {
+  if (submitted) {
+    return (
+      <>
+        <h2>Thank you!</h2>
+        <div>I'll be in touch soon.</div>
+      </>
+    );
+  }
+
   return (
     <div>
-        <h1>⦒CONTACT ME⦑</h1>
+      <h1>⦒CONTACT ME⦑</h1>
+    <form
+      onSubmit={handleSubmit}
+      method="POST"
+      target="_blank"
+    >
+      <div>
+        <input type="text" placeholder="Your name" name="name" required />
+      </div>
+      <div>
+        <input type="email" placeholder="Email" name="email" required />
+      </div>
+      <div>
+        <textarea placeholder="Write your message..." name="message" required />
+      </div>
+      <div>
+        <button type="submit"> |SEND| </button>
+      </div>
+    </form>
     </div>
   );
-}
+};
 
 export default ContactForm;
